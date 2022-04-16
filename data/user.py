9dc2d8bc6ef15691b6796268d2d07c7da0b5ac9b
Yaro373 <zakoyasse@gmail.com>
2022-04-16 13:55:52 +0300

    no

--- data/user.py
+++ data/user.py
@@ -16,7 +16,7 @@ class RolesUsers(SqlAlchemyBase):
     role_id = Column('role_id', Integer(), ForeignKey('role.id'))
 
 
-class Role(SqlAlchemyBase, RoleMixin, SerializerMixin):
+class Role(SqlAlchemyBase, RoleMixin):
     __tablename__ = 'role'
 
     id = Column(sqlalchemy.Integer(), primary_key=True)
